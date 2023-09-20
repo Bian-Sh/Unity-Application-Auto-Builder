b92d34b6bba83fb1382efca27ff9cbe70fdbe4b0
边上海 <bshsf@qq.com>
2023-09-20 22:03:21 +0800

    将 Test Run Task 按钮暴漏在所有任务中，方便测试

--- Packages/App Auto Builder/Editor/GUI/BaseTaskEditor.cs
+++ Packages/App Auto Builder/Editor/GUI/BaseTaskEditor.cs
@@ -1,12 +1,22 @@
 using UnityEditor;
-
+using UnityEngine;
+[CustomEditor(typeof(BaseTask), true)]
 public class BaseTaskEditor : Editor
 {
     public BaseTask task;
-
+    public GUIContent GUIContent = new GUIContent("Test Run Task", "测试用户任务，请务必对自己的操作有认知能力！");
     public virtual void OnEnable()
     {
         task = (BaseTask)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button(GUIContent, GUILayout.Height(30), GUILayout.Width(120)))
+        {
+            task.Run();
+        }
+        DrawHelpbox();
     }
 
     public void DrawHelpbox()
