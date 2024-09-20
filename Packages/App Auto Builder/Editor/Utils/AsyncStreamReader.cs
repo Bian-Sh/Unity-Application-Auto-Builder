7872c44e12c2b01ecb6d49e40e64eea2e284d992
雨落随风 <bshsf@qq.com>
2024-09-20 18:38:29 +0800

    [update] 使用 AsyncStreamReader 代替 ProcessOutputReader

---
+++ Packages/App Auto Builder/Editor/Utils/AsyncStreamReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace zFramework.AppBuilder.Utils
+{
+    internal class AsyncStreamReader : IDisposable 
+    {
+        private readonly Thread thread;
+        internal event Action<string> OnOutputDataReceived;
+        private readonly StreamReader reader;
+
+        public AsyncStreamReader(StreamReader reader)
+        {
+            this.reader = reader;
+            thread = new Thread(ReceivingDataFunc);
+        }
+
+        private void ReceivingDataFunc()
+        {
+            while ( reader?.BaseStream != null)
+            {
+                lock (reader)
+                {
+                    var line = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        OnOutputDataReceived?.Invoke(line);
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Start()
+        {
+            thread.Start();
+        }
+
+        public void Dispose()
+        {
+            lock (reader)
+            {
+                reader.Dispose();
+            }
+            thread.Join();
+        }
+    }
+
+}
