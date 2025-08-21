484817435aeb9fdcf730bede48123492f39ed810
bianshanghai <bshsf@qq.com>
2025-08-21 19:35:24 +0800

    对 BuildProfiles 外部输入数据进行去空格和字符替换处理

--- Packages/App Auto Builder/Editor/Data/BuildProfiles.cs
+++ Packages/App Auto Builder/Editor/Data/BuildProfiles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace zFramework.AppBuilder
 {
     [Serializable]
-    public class BuildProfiles
+    public class BuildProfiles : ISerializationCallbackReceiver
     {
         [Header("应用名称：")]
         public string productName;
@@ -12,16 +12,27 @@ namespace zFramework.AppBuilder
         public bool isBuild;
         [Header("打包平台")]
         public Platform platform;
-        [Header("保存路径（Sub）"),ShowInExplorer]
+        [Header("保存路径（Sub）"), ShowInExplorer]
         public string saveLocation;
         [Header("软件版本：（形如：1.0.0）")]
         public string productVersion;
         [Header("构建可选项："), EnumFlags]
-		public BuildOptionsLit buildOptions;
-		[Header("场景列表：不勾选不打包，注意排序")]
+        public BuildOptionsLit buildOptions;
+        [Header("场景列表：不勾选不打包，注意排序")]
         public List<SceneInfo> scenes;
         [Header("用户自定义任务")]
         public List<TaskInfo> customTask;
 
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            // 序列化之前，休整哪些首尾一定不可以存在空格的数据
+            productName = productName?.Trim();
+            saveLocation = saveLocation?.Trim();
+            productVersion = productVersion?.Trim();
+        }
     }
 }
